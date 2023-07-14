04fb0b727ddf95b5c514d3a94ed92480defca1f8
Authachai <authachai0936183311@gmail.com>
2023-07-14 17:44:03 +0700

    add_room_alert_add_user

--- project/models.py
+++ project/models.py
@@ -6,3 +6,9 @@ class Users(models.Model):
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=100)
     room_num = models.CharField(max_length=255)
+
+class Rooms(models.Model):
+    room_number = models.CharField(max_length=3)
+    room_capacity = models.CharField(max_length=1)
+
+
